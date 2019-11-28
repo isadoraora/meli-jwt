6b2aab2c4acb9af9df87b3f2c5dc3506fe7ad2a2
isadoraora <isa.mdeo@gmail.com>
2019-11-27 21:06:23 -0300

    autenticacao e jwt

--- src/routes/alunasRoute.js
+++ src/routes/alunasRoute.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/alunasController")
+const authMiddleware = require('../middlewares/auth')
 
 router.get("/", controller.get)
+//somente a rota / está exposta, as outras para termos retorno precisamos de um token válido
+router.use(authMiddleware);
 router.get("/nasceuSp", controller.getSp)
 router.get("/:id", controller.getById)
 router.get("/:id/books", controller.getBooks)
